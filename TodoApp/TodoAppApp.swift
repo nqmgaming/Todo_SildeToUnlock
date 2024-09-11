62ce659357f1f52ebdce032b29c5b19a132e42ec
Nguyen Quang Minh <nguyenquangminh570@gmail.com>
2024-09-11 16:18:45 +0700

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Nguyen Quang Minh on 11/9/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
