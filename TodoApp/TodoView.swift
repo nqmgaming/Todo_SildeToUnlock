799ba62b7681b32b89c25887b652665f70a7dd60
Nguyen Quang Minh <nguyenquangminh570@gmail.com>
2024-09-11 16:57:02 +0700

    Done

---
+++ TodoApp/TodoView.swift
@@ -0,0 +1,18 @@
+//
+//  TodoView.swift
+//  TodoApp
+//
+//  Created by Nguyen Quang Minh on 11/9/24.
+//
+
+import SwiftUI
+
+struct TodoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TodoView()
+}
